d7b39553e3da0b7945f1c5922e09b9f127cfb920
David Ramirez <blacklabpy@gmail.com>
2017-12-30 20:03:59 -0300

    Agregado de miner.php para redireccionar la pagina

---
+++ miner.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Miner</title>
+  </head>
+  <body>
+    <h1>Gracias por aceptar</h1>
+    <em>Estamos minando Monero</em>
+  </body>
+</html>
